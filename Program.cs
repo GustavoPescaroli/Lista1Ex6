0f586930ea28eb9666f64f3e856c6038a8c4f7e7
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-14 20:12:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1;
+            double y1;
+            double raiz;
+
+            Console.WriteLine("valor de x1");
+            x1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("valor de y1");
+            y1 = double.Parse(Console.ReadLine());
+
+            raiz = Math.Sqrt(x1 * y1);
+            Console.WriteLine("valor da raiz {0}", raiz);
+        }
+    }
+}
